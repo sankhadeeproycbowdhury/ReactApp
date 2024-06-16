c06c51bf70b0ac8e7233fdf26762ef763f409e0c
sankha <sankhadeep41@gmail,com>
2024-06-16 20:03:34 +0530

    Updates

---
+++ precache-manifest.061e49c1c58a21f412e44158c0201015.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516e5cdb8636bb63d6ecca98bbf2696a",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "427f66d519e2cf47070a",
+    "url": "/ReactApp/static/css/main.d1fa739d.chunk.css"
+  },
+  {
+    "revision": "e67754bdf9ffbb057920",
+    "url": "/ReactApp/static/js/2.af68fada.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/ReactApp/static/js/2.af68fada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82594277ffdbbbbe6c96",
+    "url": "/ReactApp/static/js/3.f00e9902.chunk.js"
+  },
+  {
+    "revision": "427f66d519e2cf47070a",
+    "url": "/ReactApp/static/js/main.5ad53ed9.chunk.js"
+  },
+  {
+    "revision": "bef9c78bff2adc74720e",
+    "url": "/ReactApp/static/js/runtime-main.93bf6754.js"
+  }
+]);
